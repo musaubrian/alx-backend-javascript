43d3d931c340b08ca8d2bda682ff20e9072116fd
musaubrian <musaubrian15@gmail.com>
2022-12-19 21:27:24 +0300

    chore: match codestyle

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,9 @@
-export default function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+export default function getNeighborhoodsList () {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-    const self = this;
-    this.addNeighborhood = (newNeighbourhood) => {
-        self.sanFranciscoNeighborhoods.push(newNeighbourhood);
-        return self.sanFranciscoNeighborhoods
-
-    }
+  const self = this;
+  this.addNeighborhood = (newNeighbourhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighbourhood);
+    return self.sanFranciscoNeighborhoods;
+  };
 }
--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,3 @@
-export default function returnHowManyArguments(...argsPassed){
-    return argsPassed.length
+export default function returnHowManyArguments (...argsPassed) {
+  return argsPassed.length;
 }
