3ad82c27f6c3c30938180bfee32608ccc3a18d20
musaubrian <musaubrian15@gmail.com>
2023-01-04 22:53:52 +0300

    chore: classes in TS

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,4 +11,29 @@ interface Directors extends Teacher {
     numberOfReports: number
 }
 
-const printTeacher = (firstName: string, lastName: string): string => `${firstName.charAt(0)}.${lastName}`
+const printTeacher = (firstName: string, lastName: string): string => `${firstName.charAt(1)}.${lastName}`
+
+
+interface studentClassInterface {
+    firstName: string,
+    lastName: string,
+    workOnHomeWork(): string,
+    displayName(): string
+}
+
+interface studentConstructor {
+    (firstName: string, lastName: string): studentClassInterface
+}
+class StudentClass implements studentClassInterface {
+    firstName: string
+    lastName: string
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName
+    }
+
+    displayName = (): string => this.firstName;
+    workOnHomeWork = () => 'Currently working'
+
+}
