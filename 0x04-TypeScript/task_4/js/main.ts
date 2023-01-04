0b2a0f6708c93920082dd2718ac9c06161a94a06
musaubrian <musaubrian15@gmail.com>
2023-01-05 01:52:28 +0300

    refactor: remove unnecessary if statements

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -18,21 +18,18 @@ export const cTeacher: Teacher = {
     experienceTeachingC: 10
 }
 
-if (cpp) {
-    console.log('C++')
-    cpp.setTeacher(cTeacher)
-    console.log(cpp.getRequirements())
-    console.log(cpp.getAvailableTeacher())
-}
-if (java) {
-    console.log('Java')
-    java.setTeacher(cTeacher)
-    console.log(java.getRequirements())
-    console.log(java.getAvailableTeacher())
-}
-if (react) {
-    console.log('React')
-    react.setTeacher(cTeacher)
-    console.log(react.getRequirements())
-    console.log(react.getAvailableTeacher())
-}
+
+console.log('C++')
+cpp.setTeacher(cTeacher)
+console.log(cpp.getRequirements())
+console.log(cpp.getAvailableTeacher())
+
+console.log('Java')
+java.setTeacher(cTeacher)
+console.log(java.getRequirements())
+console.log(java.getAvailableTeacher())
+
+console.log('React')
+react.setTeacher(cTeacher)
+console.log(react.getRequirements())
+console.log(react.getAvailableTeacher())
