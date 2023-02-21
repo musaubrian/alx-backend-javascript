9c819d892046ee92d4c64d5f38075ec1dd3f9545
Ernest <musaubrian15@gmail.com>
2023-02-21 19:15:44 +0300

    chore: simple express server

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+const port = 1245;
+app.listen(port);
+
+module.exports = app;
