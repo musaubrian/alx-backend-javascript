b873c4129c4ea85cd65abad4a3672b6edf31d00d
Ernest <musaubrian15@gmail.com>
2023-02-24 15:13:01 +0300

    chore: use sinon's stub to test

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,15 +1,17 @@
-const sendPaymentRequestToApi = require('./3-payment.js');
+const sendPaymentRequestToApi = require('./4-payment.js');
 const Utils = require('./utils');
 const sinon = require('sinon');
 const chai = require('chai');
 const expect = chai.expect;
 
 describe('PaymentApi', function () {
-  it('always returns the same number: 10', function () {
-    const callback = sinon.spy(Utils, 'calculateNumber');
+  it('uses calculateNumber', function () {
+    const callback = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const csl = sinon.spy(console, 'log');
     sendPaymentRequestToApi(200, 400);
-    expect(callback.calledOnce).to.be.true;
     expect(callback.calledWith('SUM', 200, 400)).to.be.true;
+    expect(csl.calledOnceWithExactly('The total is: 10'));
     callback.restore();
+    csl.restore();
   });
 });
