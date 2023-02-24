ebdb20962bac6f469630f34a897e9db342443fed
Ernest <musaubrian15@gmail.com>
2023-02-24 16:04:34 +0300

    chore: use hooks
    use beforeEach and afterEach hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,31 @@
+const sendPaymentRequestToAPI = require('./5-payment.js');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('use before and after hooks', function () {
+  let callback;
+  let csl;
+
+  beforeEach(function () {
+    callback = sinon.stub(Utils, 'calculateNumber').returns(10);
+    csl = sinon.spy(console, 'log');
+  });
+
+  afterEach(function () {
+    callback.restore();
+    csl.restore();
+  });
+
+  it('call sendPaymentRequestToAPI with 100, and 20:', () => {
+    sendPaymentRequestToAPI(100, 20);
+    expect(callback.calledWith('SUM', 100, 20)).to.be.true;
+    expect(csl.calledOnceWithExactly('The total is: 120'));
+  });
+  it('call sendPaymentRequestToAPI with 10, and 10', function () {
+    sendPaymentRequestToAPI(10, 10);
+    expect(callback.calledWith('SUM', 10, 10)).to.be.true;
+    expect(csl.calledOnceWithExactly('The total is: 20'));
+  });
+});
